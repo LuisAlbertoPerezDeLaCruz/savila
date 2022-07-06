9a6f80397fc875ad9c1a27a9618e86a9a50f0075
Luis Alberto Perez <luisalbertoperez@gmail.com>
2022-07-06 12:44:47 +0200

    Incorporada funcionalidad para ajax

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import login as auth_login
-from django.contrib.auth.forms import UserCreationForm
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 
 from .forms import SignUpForm
 
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from accounts.models import User
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import UpdateView
@@ -21,6 +22,14 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
+
+def validate_username(request):
+    username = request.GET.get('username', None)
+    data = {
+        'is_taken': User.objects.filter(username__iexact=username).exists()
+    }
+    return JsonResponse(data)
 
 
 @method_decorator(login_required, name='dispatch')
--- savila/urls.py
+++ savila/urls.py
@@ -30,6 +30,8 @@ urlpatterns = [
     # auth urls
     #
     path('signup/', accounts_views.signup, name='signup'),
+    path('ajax/validate_username/',
+         accounts_views.validate_username, name='validate_username'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'),
          name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
