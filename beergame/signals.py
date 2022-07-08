2dc3e79c23fedee1171789afce31e4339f3fc777
Luis Alberto Perez <luisalbertoperez@gmail.com>
2022-07-08 06:16:19 +0200

    avance en signals

--- beergame/signals.py
+++ beergame/signals.py
@@ -1,24 +1,17 @@
 # savila/signals.py
 
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Game, GamePlayer, TokenForRefresh
 import uuid
 
 
 @receiver(post_save, sender=Game)
-def save_game(sender, instance, created, **kwargs):
-    if created:
-        token = str(uuid.uuid4())
-        obj = TokenForRefresh.objects.create(token=token)
-        TokenForRefresh.objects.exclude(pk=obj.pk).delete()
-        pass
-
-
 @receiver(post_save, sender=GamePlayer)
-def save_game_player(sender, instance, created, **kwargs):
-    if created:
-        token = str(uuid.uuid4())
-        obj = TokenForRefresh.objects.create(token=token)
-        TokenForRefresh.objects.exclude(pk=obj.pk).delete()
-        pass
+@receiver(post_delete, sender=Game)
+@receiver(post_delete, sender=GamePlayer)
+def save_game_player(sender, **kwargs):
+    token = str(uuid.uuid4())
+    obj = TokenForRefresh.objects.create(token=token)
+    TokenForRefresh.objects.exclude(pk=obj.pk).delete()
+    return
