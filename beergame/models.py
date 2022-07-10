6d48d6baea5207418592e19fd6aecdeb5a5be4ad
Luis Alberto Perez <luisalbertoperez@gmail.com>
2022-07-10 12:56:26 +0200

    ajustes

--- beergame/models.py
+++ beergame/models.py
@@ -1,6 +1,7 @@
 import json
 from django.db import models
 from accounts.models import User
+from datetime import datetime
 
 
 class Game(models.Model):
@@ -21,6 +22,15 @@ class Game(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     max_turns = models.IntegerField(default=25,)
+
+    last_play_at = models.DateTimeField(null=True)
+
+    last_play_by = models.ForeignKey(
+        User, related_name='last_plays', null=True, on_delete=models.CASCADE)
+    next_play_by = models.ForeignKey(
+        User, related_name='next_plays', null=True, on_delete=models.CASCADE)
+
+    turns_played = models.IntegerField(default=0)
 
     def playerslist(self):
         rels = self.players.all()
@@ -50,7 +60,7 @@ class GamePlayer(models.Model):
 
 class GameTurn(models.Model):
     turn = models.IntegerField(default=0,)
-    player = models.ForeignKey(
+    game_player = models.ForeignKey(
         GamePlayer, null=True, related_name='gameplayers', on_delete=models.CASCADE)
     value_played = models.DecimalField(
         decimal_places=2, default=0, max_digits=12,
@@ -64,8 +74,49 @@ class GameTurn(models.Model):
     def get_turn_result(self):
         return json.loads(self.turn_result)
 
+    def save(self,  *args, **kwargs):
+        self.game_player.last_play_at = datetime.now()
+
+        # Si el objecto self no esta creado, entonces el pk en nulo,
+        # lo cual quiere decir que estoy creando el objeto y
+        # no actualizandolo
+
+        if self.pk is None:
+            self.turn += 1
+            # A traves del la relacion game_player modifico el objeto game
+            # adicionandole loa valores: last_play_at, last_play_by, next_play_by
+            # y turns_played
+
+            self.game_player.game.last_play_at = datetime.now()
+            self.game_player.game.turns_played = self.turn
+            self.game_player.game.last_play_by = self.game_player.player
+
+            _ = self.game_player.game.playerslist().strip().split()
+            curr_idx = _.index(self.game_player.player.username)
+            next_idx_ = curr_idx + 1
+
+            if next_idx_ > len(_)-1:
+                next_idx = 0
+            else:
+                next_idx = next_idx_
+
+            next_player = User.objects.get(username=_[next_idx])
+
+            self.game_player.game.next_play_by = next_player
+            self.game_player.game.save()
+        else:
+            # Automaticamente calculo los turns_played, buscando los turns que
+            # pertenecen al mismo game
+            turns_played = self.__class__.objects.filter(
+                game_player__game=self.game_player.game).count()
+            self.game_player.game.turns_played = turns_played
+            self.game_player.game.save()
+
+        self.game_player.save()
+        super().save(*args,  **kwargs)
+
     def __str__(self):
-        return f'{self.turn},{self.player.player.username}, {self.value_played}, {self.turn_result}'
+        return f'{self.turn},{self.game_player.player.username}, {self.value_played}, {self.turn_result}'
 
 
 class TokenForRefresh(models.Model):
--- beergame/signals.py
+++ beergame/signals.py
@@ -2,16 +2,26 @@
 
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import Game, GamePlayer, TokenForRefresh
+from .models import Game, GamePlayer, GameTurn, TokenForRefresh
 import uuid
 
 
 @receiver(post_save, sender=Game)
 @receiver(post_save, sender=GamePlayer)
+@receiver(post_save, sender=GameTurn)
 @receiver(post_delete, sender=Game)
 @receiver(post_delete, sender=GamePlayer)
+@receiver(post_delete, sender=GameTurn)
 def save_game_player(sender, **kwargs):
     token = str(uuid.uuid4())
     obj = TokenForRefresh.objects.create(token=token)
     TokenForRefresh.objects.exclude(pk=obj.pk).delete()
+    return
+
+
+@receiver(post_delete, sender=GameTurn)
+def delete_game_turn(sender, **kwargs):
+    last_instance = GameTurn.objects.last()
+    if last_instance:
+        last_instance.save()
     return
