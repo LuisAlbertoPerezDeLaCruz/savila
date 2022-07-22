6718ad12918cc11716fbbc70b9088d9e6faf3c8f
Luis Alberto Perez <luisalbertoperez@gmail.com>
2022-07-22 11:03:19 +0200

    ajustes

--- accounts/models.py
+++ accounts/models.py
@@ -5,5 +5,7 @@ from django.db import models
 class User(AbstractUser):
     is_instructor = models.BooleanField(default=False)
 
+    username = models.CharField(max_length=8, unique=True)
+
     def __str__(self):
         return self.username
