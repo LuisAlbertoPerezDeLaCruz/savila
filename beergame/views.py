7c07823729ce7df57b680eb62fe46e31b4a6cfda
Luis Alberto Perez <luisalbertoperez@gmail.com>
2022-07-26 20:41:18 +0200

    avance

--- beergame/views.py
+++ beergame/views.py
@@ -267,38 +267,44 @@ def game_finished(request, pk):
     game_turns = GameTurn.objects.filter(
         game_player__game=pk).order_by('turn')
 
-    result_data = [
-        [1, 13, 0, 2, 3, 21, 25, 26],
-        [4, 14, 17, 5, 6, 22, 25, 26],
-        [7, 15, 18, 8, 9, 23, 25, 26],
-        [10, 16, 19, 11, 12, 24, 25, 26],
-    ]
-    for game_turn in game_turns:
-        pos = -1
-        for idx, player in enumerate(game_players):
-            if game_turn.game_player == player:
-                pos = idx
-                break
-        game_turn.role = roles[pos]
-        game_turn.value_played = int(game_turn.value_played)
-        round_result = json.loads(game_turn.round_result)
-        game_turn.inventory = round_result[result_data[pos][0]]
-        game_turn.backlog = round_result[result_data[pos][1]]
-        game_turn.order_client = round_result[result_data[pos][2]]
-        game_turn.received_product = round_result[result_data[pos][3]]
-        game_turn.to_receive = round_result[result_data[pos][4]]
-        game_turn.round_pos_cost = round_result[result_data[pos][5]]
-        game_turn.round_cost = round_result[result_data[pos][6]]
-        game_turn.cumulative_cost = round_result[result_data[pos][7]]
+    list_result = list()
+    for idx, game_turn in enumerate(game_turns):
+        changed_round = (idx+1) % 4 == 0
+        if changed_round:
+            list_row = list()
+            round_result = json.loads(game_turn.round_result)
+
+            list_row.append(game_turn.round)
+
+            # retailer data
+            list_row.append(round_result[1])  # Inventory
+            list_row.append(round_result[13])  # Backlog
+
+            # wholesaler data
+            list_row.append(round_result[4])  # Inventory
+            list_row.append(round_result[14])  # Backlog
+
+            # distributor data
+            list_row.append(round_result[7])  # Inventory
+            list_row.append(round_result[15])  # Backlog
+
+            # manufacturer data
+            list_row.append(round_result[10])  # Inventory
+            list_row.append(round_result[16])  # Backlog
+
+            # Acumulator
+            list_row.append(round_result[26])  # Backlog
+
+            list_result.append(list_row)
 
     view = 'game_finished_view.html'
 
     return render(request, view, {"game": game,
                                   "players": players,
                                   "players_list": players_list,
-                                  "game_turns": game_turns,
                                   "institution": institution,
-                                  "institutions": institutions})
+                                  "institutions": institutions,
+                                  "list_result": list_result})
 
 
 def pos_description(pos):
