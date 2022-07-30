2ecc3b22b29368dd10d3dac756b4ead008c4c73b
Luis Alberto Perez <luisalbertoperez@gmail.com>
2022-07-30 09:28:02 +0200

    ajuste

--- savila/settings.py
+++ savila/settings.py
@@ -17,6 +17,8 @@ from decouple import config, Csv
 
 from django.contrib.messages import constants as messages
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,13 +86,18 @@ WSGI_APPLICATION = 'savila.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
